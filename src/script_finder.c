93632f7a0c575b1147e7345df29e1cdf79059cd5
Till Schröder <hi@till.red>
2021-04-25 14:15:14 +0200

    add emacs config & leak less memory

--- src/script_finder.c
+++ src/script_finder.c
@@ -6,20 +6,21 @@
 #include <string.h>
 #include <sys/types.h>
 
-sds find_script(sds filename, sds scripts_dir) {
-  sds file_extension = get_filename_ext(filename);
+sds find_script(char* filename, char* scripts_dir) {
+  char* file_extension = get_filename_ext(filename);
   printf("filename: %s\n", filename);
   printf("scripts dir: %s\n", scripts_dir);
   DIR *dir = opendir(scripts_dir);
-  struct dirent *entry;
-  if (dir == NULL) {
-    return sdsempty();
-  } else {
+  if (dir != NULL) {
+    struct dirent *entry;
     while ((entry = readdir(dir))) {
       if (memcmp(file_extension, entry->d_name, sdslen(file_extension)) == 0) {
-        return sdscat(sdscat(scripts_dir, "/"), entry->d_name);
+        closedir(dir);
+        return sdscat(s(scripts_dir, "/"), entry->d_name);
       }
     }
   }
+
+  closedir(dir);
   return sdsempty();
 }
