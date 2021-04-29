a5615ccb56d44923d12f0687f62d212e21642634
Till <till.schroder@insurello.se>
2021-04-29 22:59:22 +0200

    format tests

--- tests/test.c
+++ tests/test.c
@@ -7,9 +7,7 @@ CESTER_TEST(get_filename_ext, inst,
             cester_assert_str_equal(get_filename_ext("video.cool.mp4"), "mp4");
             cester_assert_str_equal(get_filename_ext("Makefile"), "Makefile");)
 
-
-CESTER_TEST(check_match_macro, inst,
-            cester_assert_true(MATCH("test", "test"));
+CESTER_TEST(check_match_macro, inst, cester_assert_true(MATCH("test", "test"));
             cester_assert_true(MATCH("", ""));
             cester_assert_true(MATCH("123456789", "123456789"));
             cester_assert_false(MATCH("test", "nope"));
